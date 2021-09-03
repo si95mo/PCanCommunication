53f1c6d40070c02aa928aa89f89a3f75300371f3
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-09-03 11:42:44 +0200

    Add missing functionalities (baud rate, log filtering, and a (static) method to acquire the connected device handle

--- Hardware.Can.Peak/PeakCanResource.cs
+++ Hardware.Can.Peak/PeakCanResource.cs
@@ -8,6 +8,83 @@ using System.Threading.Tasks;
 namespace Hardware.Can
 {
     /// <summary>
+    /// Define the available <see cref="PeakCanResource"/>
+    /// baud-rates (bit-rates)
+    /// </summary>
+    public enum BaudRate : ushort
+    { 
+        /// <summary>
+        /// 1 MBit/s
+        /// </summary>
+        K1000 = 0x0014,
+
+        /// <summary>
+        /// 800 KBit/s
+        /// </summary>
+        K800 = 0x0016,
+
+        /// <summary>
+        /// 500 kBit/s
+        /// </summary>
+        K500 = 0x001C,
+
+        /// <summary>
+        /// 250 kBit/s
+        /// </summary>
+        K250 = 0x011C,
+
+        /// <summary>
+        /// 125 kBit/s
+        /// </summary>
+        K125 = 0x031C,
+
+        /// <summary>
+        /// 100 kBit/s
+        /// </summary>
+        K100 = 0x432F,
+
+        /// <summary>
+        /// 95,238 KBit/s
+        /// </summary>
+        K95 = 0xC34E,
+
+        /// <summary>
+        /// 83,333 KBit/s
+        /// </summary>
+        K83 = 0x852B,
+
+        /// <summary>
+        /// 50 kBit/s
+        /// </summary>
+        K50 = 0x472F,
+
+        /// <summary>
+        /// 47,619 KBit/s
+        /// </summary>
+        K47 = 0x1414,
+
+        /// <summary>
+        /// 33,333 KBit/s
+        /// </summary>
+        K33 = 0x8B2F,
+
+        /// <summary>
+        /// 20 kBit/s
+        /// </summary>
+        K20 = 0x532F,
+
+        /// <summary>
+        /// 10 kBit/s
+        /// </summary>
+        K10 = 0x672F,
+
+        /// <summary>
+        /// 5 kBit/s
+        /// </summary>
+        K5 = 0x7F7F,
+    }
+
+    /// <summary>
     /// Handles the property status changed event.
     /// See also <see cref="EventArgs"/>
     /// </summary>
@@ -49,9 +126,10 @@ namespace Hardware.Can
         private bool logEnabled;
 
         private object objectLock = new object();
+        private object logLock = new object();
 
         private Task rxTask;
-        private bool isReceiving;
+        private bool started;
         private AutoResetEvent receiveEvent;
 
         private delegate void ReadHandler();
@@ -59,6 +137,17 @@ namespace Hardware.Can
         private ReadHandler readHandler;
 
         private ushort channelHandle;
+        private byte hwType;
+        private uint ioPort;
+        private ushort interrupt;
+
+        private Dictionary<int, bool> filteredCanId;
+
+        /// <summary>
+        /// The <see cref="Dictionary{TKey, TValue}"/> with
+        /// the filtered can id
+        /// </summary>
+        public Dictionary<int, bool> FilteredCanId => filteredCanId;
 
         public EventHandler<StatusChangedEventArgs> StatusChangedHandler;
 
@@ -114,8 +203,8 @@ namespace Hardware.Can
         /// <summary>
         /// Create a new instance of <see cref="PeakCanResource"/>
         /// and perform an attribute initialization. <br/>
-        /// For a full initialization, use <see cref="PeakCanResource(ushort, ushort)"/>
-        /// or <see cref="PeakCanResource(ushort, ushort, byte, uint, ushort)"/>
+        /// For a full initialization, use <see cref="PeakCanResource(ushort, BaudRate)"/>
+        /// or <see cref="PeakCanResource(ushort, BaudRate, byte, uint, ushort)"/>
         /// </summary>
         public PeakCanResource()
         {
@@ -125,29 +214,31 @@ namespace Hardware.Can
             maxCapacity = 65535;
             logEnabled = false;
 
-            isReceiving = false;
+            started = false;
             receiveEvent = new AutoResetEvent(false);
             readHandler = new ReadHandler(ReadMessages);
 
             rxTask = default; // Initialization to a known state
+
+            filteredCanId = new Dictionary<int, bool>();
         }
 
         /// <summary>
         /// Create a new instance of <see cref="PeakCanResource"/>
         /// for non plug-and-play hardware. <br/>
-        /// See also <see cref="PeakCanResource(ushort, ushort)"/>
+        /// See also <see cref="PeakCanResource(ushort, BaudRate)"/>
         /// </summary>
         /// <param name="hwChannel">The channel handle</param>
-        /// <param name="baudRate">The baud rate</param>
+        /// <param name="baudRate">The <see cref="BaudRate"/></param>
         /// <param name="hwType">The hardware type</param>
         /// <param name="ioPort">The IO address of the parallel port</param>
         /// <param name="interrupt">The interrupt number of the parallel port</param>
         /// <remarks>
         /// This constructor should only be called for the non plug-and-play hardware
         /// (such as PCAN-Dongle or PCAN-ISA). <br/>
-        /// For plug-and-play hardware see <see cref="PeakCanResource(ushort, ushort)"/>
+        /// For plug-and-play hardware see <see cref="PeakCanResource(ushort, BaudRate)"/>
         /// </remarks>
-        public PeakCanResource(ushort hwChannel, ushort baudRate, byte hwType, uint ioPort, ushort interrupt) : this()
+        public PeakCanResource(ushort hwChannel, BaudRate baudRate, byte hwType, uint ioPort, ushort interrupt) : this()
         {
             Status = (uint)PCANBasic.Initialize(
                 hwChannel,
@@ -156,22 +247,26 @@ namespace Hardware.Can
                 ioPort,
                 interrupt
             );
+
             channelHandle = hwChannel;
+            this.hwType = hwType;
+            this.ioPort = ioPort;
+            this.interrupt = interrupt;
         }
 
         /// <summary>
         /// Create a new instance of <see cref="PeakCanResource"/>
         /// for plug-and-play hardware. <br/>
-        /// See also <see cref="PeakCanResource(ushort, ushort, byte, uint, ushort)"/>
+        /// See also <see cref="PeakCanResource(ushort, BaudRate, byte, uint, ushort)"/>
         /// </summary>
         /// <param name="hwChannel">The channel handle</param>
-        /// <param name="baudRate">The baud rate</param>
+        /// <param name="baudRate">The <see cref="BaudRate"/></param>
         /// <remarks>
         /// This constructor should only be called for the plug-and-play hardware
         /// (such as PCAN-USB, PCAN-USB Pro or PCAN-PCI). <br/>
-        /// For non plug-and-play hardware see <see cref="PeakCanResource(ushort, ushort, byte, uint, ushort)"/>
+        /// For non plug-and-play hardware see <see cref="PeakCanResource(ushort, BaudRate, byte, uint, ushort)"/>
         /// </remarks>
-        public PeakCanResource(ushort hwChannel, ushort baudRate) : this(hwChannel, baudRate, 0, 0, 0)
+        public PeakCanResource(ushort hwChannel, BaudRate baudRate) : this(hwChannel, baudRate, 0, 0, 0)
         { }
 
         /// <summary>
@@ -179,7 +274,7 @@ namespace Hardware.Can
         /// </summary>
         public void Start()
         {
-            isReceiving = true;
+            started = true;
 
             if (rxTask == default || rxTask.IsCompleted)
             {
@@ -193,14 +288,10 @@ namespace Hardware.Can
         /// </summary>
         public void Stop()
         {
-            isReceiving = false;
+            started = false;
 
             if (rxTask != default && !rxTask.IsCompleted)
-            {
-                // First, wait for the task to complete the current iteration
-                rxTask.Wait();
-                rxTask.Dispose();
-            }
+                rxTask.Wait(100);
 
             rxTask = default;
         }
@@ -215,14 +306,19 @@ namespace Hardware.Can
         /// </returns>
         public bool Send(CanFrame canFrame)
         {
-            TPCANMsg msg = new TPCANMsg();
-            msg.ID = (uint)canFrame.Id;
-            msg.MSGTYPE = TPCANMessageType.PCAN_MESSAGE_STANDARD;
-            msg.LEN = 8;
-            msg.DATA = canFrame.Data;
+            bool succeeded = true;
 
-            Status = (uint)PCANBasic.Write(channelHandle, ref msg);
-            bool succeeded = status == (uint)TPCANStatus.PCAN_ERROR_OK;
+            if (started)
+            {
+                TPCANMsg msg = new TPCANMsg();
+                msg.ID = (uint)canFrame.Id;
+                msg.MSGTYPE = TPCANMessageType.PCAN_MESSAGE_STANDARD;
+                msg.LEN = 8;
+                msg.DATA = canFrame.Data;
+
+                Status = (uint)PCANBasic.Write(channelHandle, ref msg);
+                succeeded = status == (uint)TPCANStatus.PCAN_ERROR_OK;
+            }
 
             return succeeded;
         }
@@ -254,10 +350,19 @@ namespace Hardware.Can
 
                 if (logEnabled)
                 {
-                    if (logQueue.Count >= maxCapacity)
-                        logQueue.Dequeue();
+                    if (filteredCanId.TryGetValue(canFrame.Id, out bool enabled))
+                    {
+                        if (enabled)
+                        {
+                            lock (logLock)
+                            {
+                                if (logQueue.Count >= maxCapacity)
+                                    logQueue.Dequeue();
 
-                    logQueue.Enqueue(canFrame);
+                                logQueue.Enqueue(canFrame);
+                            }
+                        }
+                    }
                 }
 
                 for (int i = 0; i < channels.Count && !channelFound; i++)
@@ -266,10 +371,11 @@ namespace Hardware.Can
                     if (channel.CanId == message.ID)
                     {
                         channelFound = true;
+                        channel.Data = canFrame.Data;
                         channel.CanFrame = canFrame;
                     }
                 }
-            } while (isReceiving && !Convert.ToBoolean(readResult & TPCANStatus.PCAN_ERROR_QRCVEMPTY));
+            } while (started && !Convert.ToBoolean(readResult & TPCANStatus.PCAN_ERROR_QRCVEMPTY));
         }
 
         /// <summary>
@@ -291,7 +397,7 @@ namespace Hardware.Can
                     if (setValueResult != TPCANStatus.PCAN_ERROR_OK)
                         Status = (uint)setValueResult;
 
-                    while (isReceiving)
+                    while (started)
                     {
                         if (receiveEvent.WaitOne(50))
                             readHandler.Invoke();
@@ -304,14 +410,21 @@ namespace Hardware.Can
         /// <summary>
         /// Set the <see cref="PeakCanResource"/> baud rate
         /// </summary>
-        /// <param name="baudRate">The baud rate to set</param>
+        /// <param name="baudRate">The <see cref="BaudRate"/> to set</param>
         /// <returns>
         /// <see langword="true"/> if the operation succeeded,
         /// <see langword="false"/> otherwise
         /// </returns>
-        public bool SetBaudRate(uint baudRate)
+        public bool SetBaudRate(BaudRate baudRate)
         {
-            throw new NotImplementedException();
+            bool succeeded = true;
+
+            Status = (uint)PCANBasic.Uninitialize(channelHandle);
+            Status = (uint)PCANBasic.Initialize(channelHandle, (TPCANBaudrate)baudRate, (TPCANType)hwType, ioPort, interrupt);
+
+            succeeded &= status == (uint)TPCANStatus.PCAN_ERROR_OK;
+
+            return succeeded;
         }
 
         /// <summary>
@@ -342,7 +455,8 @@ namespace Hardware.Can
         public void DisableLog() => logEnabled = false;
 
         /// <summary>
-        /// Read the current log for received <see cref="CanFrame"/>. <br/>
+        /// Read the current log of the received <see cref="CanFrame"/>
+        /// and then empty the log queue. <br/>
         /// See also <see cref="EnableLog(int)"/> and <see cref="DisableLog"/>
         /// </summary>
         /// <returns>
@@ -357,11 +471,109 @@ namespace Hardware.Can
             // EnableLog(int) should has been called at least once
             if (logQueue != null)
             {
-                foreach (CanFrame canFrame in logQueue)
-                    log += $"{canFrame}{Environment.NewLine}";
+                lock (logLock)
+                {
+                    foreach (CanFrame canFrame in logQueue)
+                        log += $"{canFrame}{Environment.NewLine}";
+
+                    logQueue.Clear();
+                }
             }
 
             return log;
+        }
+
+        /// <summary>
+        /// Add or enable a can id into the filtered collection
+        /// </summary>
+        /// <param name="canId">The can id to add</param>
+        public void AddFilteredCanId(int canId)
+        {
+            if (filteredCanId.ContainsKey(canId))
+                filteredCanId[canId] = true;
+            else
+                filteredCanId.Add(canId, true);
+        }
+
+        /// <summary>
+        /// Remove or disable a can id into the filtered collection
+        /// </summary>
+        /// <param name="canId">The can id to remove</param>
+        public void RemoveFilteredCanId(int canId)
+        {
+            if (filteredCanId.ContainsKey(canId))
+                filteredCanId.Remove(canId);
+            else
+                filteredCanId.Add(canId, false);
+        }
+
+        /// <summary>
+        /// Get all the available hardware
+        /// </summary>
+        /// <returns>The available hardware handle and its name</returns>
+        public static List<string> GetAvailableHardware()
+        {
+            List<string> hardwareNames = new List<string>();
+
+            TPCANStatus result = PCANBasic.GetValue(
+                PCANBasic.PCAN_NONEBUS, 
+                TPCANParameter.PCAN_ATTACHED_CHANNELS_COUNT, 
+                out uint channelsNumber, 
+                sizeof(uint)
+            );
+
+            if (result == TPCANStatus.PCAN_ERROR_OK)
+            {
+                TPCANChannelInformation[] info = new TPCANChannelInformation[channelsNumber];
+
+                result = PCANBasic.GetValue(
+                    PCANBasic.PCAN_NONEBUS, 
+                    TPCANParameter.PCAN_ATTACHED_CHANNELS, 
+                    info
+                );
+
+                if (result == TPCANStatus.PCAN_ERROR_OK)
+                {
+                    uint channelAvailable;
+                    foreach (TPCANChannelInformation channel in info)
+                    {
+                        channelAvailable = channel.channel_condition & PCANBasic.PCAN_CHANNEL_AVAILABLE;
+
+                        if (channelAvailable == PCANBasic.PCAN_CHANNEL_AVAILABLE)
+                            hardwareNames.Add(FormatChannelName(channel.channel_handle));
+                    }
+                }
+            }
+
+            return hardwareNames;
+        }
+
+        /// <summary>
+        /// Gets the formated text for a PCAN-Basic channel handle
+        /// </summary>
+        /// <param name="handle">PCAN-Basic Handle to format</param>
+        /// <param name="isFD">If the channel is FD capable</param>
+        /// <returns>The formatted text for a channel</returns>
+        private static string FormatChannelName(uint handle)
+        {
+            TPCANDevice devDevice;
+            byte byChannel;
+
+            // Gets the owner device and channel for a 
+            // PCAN-Basic handle
+            //
+            if (handle < 0x100)
+            {
+                devDevice = (TPCANDevice)(handle >> 4);
+                byChannel = (byte)(handle & 0xF);
+            }
+            else
+            {
+                devDevice = (TPCANDevice)(handle >> 8);
+                byChannel = (byte)(handle & 0xFF);
+            }
+
+            return string.Format("{0} {1} ({2:X2}h)", devDevice, byChannel, handle);
         }
     }
 }
