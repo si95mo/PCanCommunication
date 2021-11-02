314ed6fadb16fb098e62ee35f28ed456a68dadf5
Simone De Grandis <s.degrandis-ext@loccioni.com>
2021-11-02 12:02:25 +0100

    Fix  Scheduler bug on path when saving the result

--- Instructions.Scheduler/Scheduler.cs
+++ Instructions.Scheduler/Scheduler.cs
@@ -72,7 +72,7 @@ namespace Instructions.Scheduler
             if (path.CompareTo("") == 0)
                 path = System.IO.Path.Combine(
                     Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
-                    $"{DateTime.Now:yyyy-MM-dd:HH:mm:ss}_result.csv"
+                    "result.csv"
                 );
 
             int order = instructions.Keys.Min();
