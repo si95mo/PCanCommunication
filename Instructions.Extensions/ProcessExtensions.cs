ed035886ef1ef054cc50faafb12b108d5c04a66b
Simone De Grandis <simodg95@gmail.com>
2022-08-30 12:10:52 +0200

    Add extension methods class

---
+++ Instructions.Extensions/ProcessExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Instructions.Extensions
+{
+    public static class ProcessExtensions
+    {
+        /// <summary>
+        /// Wait for a process to end asynchronously
+        /// </summary>
+        /// <param name="process">The process to wait</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/></param>
+        /// <returns>The <see cref="Task"/> to await</returns>
+        public static Task WaitForExitAsync(this Process process, CancellationToken cancellationToken = default)
+        {
+            if (process.HasExited)
+                return Task.CompletedTask;
+
+            TaskCompletionSource<object> taskCompletionSource = new TaskCompletionSource<object>();
+
+            process.EnableRaisingEvents = true;
+            process.Exited += (sender, args) => taskCompletionSource.TrySetResult(null);
+
+            if (cancellationToken != default)
+                cancellationToken.Register(() => taskCompletionSource.SetCanceled());
+
+            return process.HasExited ? Task.CompletedTask : taskCompletionSource.Task;
+        }
+    }
+}
